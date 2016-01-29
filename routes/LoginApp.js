495df796a3c89b39e0ca53f9526752a6bf89a6e7
Ben <bjc9001@gmail.com>
2016-01-28 19:06:50 -0600

    appended git ignore/ enabled bin

--- routes/LoginApp.js
+++ routes/LoginApp.js
@@ -9,51 +9,14 @@ var loginUtil = require('LoginUtility.js')
 router.get('/', function(req, res, next) {
     res.send('Theres Nothing Here!');
 });
+
+
 router.post('/', function(req,res,next){
    var request = req.body;
 
-    if(request.username.isString()){
-         if(request.password.isString()){
-         // this is a login object, continue to verification.
-
-            if(true){// is the user valid???
-
-              if(true){//is the password valid???
-                  var id =makeid();
-                  if(enterToUKDB(request.username,id))(
-                      res.send({
-                          Verdict:true,
-                          authenticationKey:id ,
-                          KeyTTL:60,
-                      })
-                  )
-
-              }
-           }
-         }
-    }
-
+    res.send(loginUtil.validateLogin(request));
 
 });
 
-function isValidUser( username){
-
-}
-function isValidPassword(password){
-
-}
-function enterToUKDB(user,id){
-    // mongostuff also get unique key,
-}
-function makeid()
-{
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 30; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-}
 
 module.exports = router;
